5c4ca61d346db39122bbadeadfc0ab13dcd486d8
Brady Turner <bturner@nuvotechnologies.com>
2013-08-03 15:46:18 -0400

    cleanup

--- hue.rb
+++ hue.rb
@@ -28,21 +28,11 @@ class PhillipsHueClient
   end
 
   def request_bulb_list
-    request = Net::HTTP::Get.new("/api/#{@username}/lights")
-    response = @http.request(request)
-    JSON.parse response.body 
+    hue_http_get("lights")
   end
 
   def request_bulb_info( id )
-    request = Net::HTTP::Get.new("/api/#{@username}/lights/#{id}")
-    response = @http.request request
-    JSON.parse response.body
-  end
-
-  def request_bulb_state( name )
-    request = Net::HTTP::Get.new("/api/#{@username}/lights/#{name}/state")
-    response = @http.request request
-    JSON.parse response.body
+    hue_http_get "lights/#{id}"
   end
 
 end
@@ -53,9 +43,6 @@ if __FILE__==$0
   jp bulbs_response
 
   bulbs_response.each do |key,value|
-    puts value["name"]
-    #puts hue_client.request_bulb_state key 
     jp hue_client.request_bulb_info key
   end
-
 end
