de40aaa2b599df5bc07cf0e6c0f223fed79ffafe
Brady Turner <bturner@nuvotechnologies.com>
2013-08-03 15:42:22 -0400

    huebulb class & get bulb info

--- hue.rb
+++ hue.rb
@@ -4,32 +4,58 @@ require 'net/http'
 require 'uri'
 require 'json'
 
+require './huebulb'
+
 HUE_IP    = "192.168.1.28"
 USERNAME  = "tatersnakes"
 
-def jsonp ( s )
+def jp ( s )
   puts JSON.pretty_generate( s )
 end
 
 class PhillipsHueClient
+
   def initialize(ip,username)
-    @uri = URI.parse(ip)
-    @http = Net::HTTP.new(@uri.host,@uri.port)
+    @ip = ip 
     @username = username
-
+    @http = Net::HTTP.new(ip,80)
   end
 
-  def get_lights
-    uri = URI.parse("http://#{HUE_IP}/api/#{USERNAME}/lights")
-    http = Net::HTTP.new(uri.host,uri.port)
-    request = Net::HTTP::Get.new(uri.path)
-    response = http.request(request)
-    response_json = JSON.parse( response.body )
+  def hue_http_get( path )
+    request = Net::HTTP::Get.new("/api/#{@username}/#{path}")
+    response = @http.request request
+    JSON.parse response.body
   end
+
+  def request_bulb_list
+    request = Net::HTTP::Get.new("/api/#{@username}/lights")
+    response = @http.request(request)
+    JSON.parse response.body 
+  end
+
+  def request_bulb_info( id )
+    request = Net::HTTP::Get.new("/api/#{@username}/lights/#{id}")
+    response = @http.request request
+    JSON.parse response.body
+  end
+
+  def request_bulb_state( name )
+    request = Net::HTTP::Get.new("/api/#{@username}/lights/#{name}/state")
+    response = @http.request request
+    JSON.parse response.body
+  end
+
 end
 
 if __FILE__==$0
-  client = PhillipsHueClient.new(HUE_IP,USERNAME)
-  lights_response = client.get_lights
-  jsonp lights_response
+  hue_client = PhillipsHueClient.new(HUE_IP,USERNAME)
+  bulbs_response = hue_client.request_bulb_list
+  jp bulbs_response
+
+  bulbs_response.each do |key,value|
+    puts value["name"]
+    #puts hue_client.request_bulb_state key 
+    jp hue_client.request_bulb_info key
+  end
+
 end
---
+++ huebulb.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+class HueBulb
+  def initialize(id,name)
+    @id = id
+    @name = name
+  end
+end
