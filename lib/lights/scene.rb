01937ae3b955e3c60fc2d33af2b7cc997da1c2d5
Brady Turner <bradyaturner@gmail.com>
2017-03-04 13:39:40 -0500

    Added ability to set transition time when creating a scene

--- lib/lights/scene.rb
+++ lib/lights/scene.rb
@@ -1,13 +1,14 @@
 require 'lights/hobject'
 
 class Scene < HObject
-  attr_accessor :id, :name, :active, :lights, :recycle
+  attr_accessor :id, :name, :active, :lights, :recycle, :transition_time
   def initialize(id = nil,data = {})
     @id = id
     @name = data["name"]
     @active = data["active"]
     @lights = data["lights"]
     @recycle = data["recycle"]
+    @transition_time = data["transitiontime"]
   end
 
   def data
@@ -16,6 +17,7 @@ class Scene < HObject
     data["active"] = @active unless @active.nil?
     data["lights"] = @lights if @lights
     data["recycle"] = @recycle if @recycle
+    data["transitiontime"] = @transition_time if @transition_time
     data
   end
 end
